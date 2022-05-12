05a0f3fd1e05d31cd92d43711ff8c31d48a7afcb
Oliwer Stryjewski <olsskko@gmail.com>
2022-05-12 22:06:54 +0200

    Redesign of menu navigation. Remade borders around projects. Added contact me page.

--- website/javascript_website.js
+++ website/javascript_website.js
@@ -1,3 +1,12 @@
 var date = new Date();
 var year =  date.getFullYear() + " ";
 document.getElementById("currentYear").innerHTML = year;
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
